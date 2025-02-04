8c6a282a6c7d891fdc039d6be65f51e909c10e0b
alexaNovikova <alexadubinina@mail.ru>
2025-02-04 15:59:09 +0700

    HW 4, работа Garbage Collector

--- L08-gc/homework/src/main/java/ru/otus/calculator/CalcDemo.java
+++ L08-gc/homework/src/main/java/ru/otus/calculator/CalcDemo.java
@@ -9,10 +9,10 @@ package ru.otus.calculator;
 -Xlog:gc=debug:file=./logs/gc-%p-%t.log:tags,uptime,time,level:filecount=5,filesize=10m
 */
 
+import java.time.LocalDateTime;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.time.LocalDateTime;
 
 public class CalcDemo {
     private static final Logger log = LoggerFactory.getLogger(CalcDemo.class);
@@ -22,8 +22,9 @@ public class CalcDemo {
         var summator = new Summator();
         long startTime = System.currentTimeMillis();
 
+        Data data = new Data();
         for (var idx = 0; idx < counter; idx++) {
-            var data = new Data(idx);
+            data.setValue(idx);
             summator.calc(data);
 
             if (idx % 10_000_000 == 0) {
