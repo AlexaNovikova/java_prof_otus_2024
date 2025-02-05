20d95aff4a7bf2756c64b43f0741e478a6d654a1
alexaNovikova <alexadubinina@mail.ru>
2025-02-05 17:52:00 +0700

    Урок 9 Docker

---
+++ L09-docker/src/main/java/ru/otus/controller/IndexController.java
@@ -0,0 +1,33 @@
+package ru.otus.controller;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.sql.SQLException;
+import org.springframework.web.bind.annotation.*;
+import ru.otus.database.JdbcDemo;
+
+@RestController
+public class IndexController {
+
+    private final JdbcDemo jdbcDemo;
+
+    public IndexController(JdbcDemo jdbcDemo) {
+        this.jdbcDemo = jdbcDemo;
+    }
+
+    @GetMapping("/hi")
+    public String hi(@RequestParam(name = "name") String name) throws UnknownHostException {
+        return String.format(
+                "Hi, %s. It works, host: %s", name, InetAddress.getLocalHost().getHostName());
+    }
+
+    @PostMapping("/response/{name}")
+    public Response response(@PathVariable("name") String name, @RequestBody Request params) {
+        return new Response(name, String.format("%s-%s", params.param1(), params.param2()));
+    }
+
+    @GetMapping("/jdbcDemo")
+    public long jdbcDemo() throws SQLException {
+        return jdbcDemo.exec();
+    }
+}
