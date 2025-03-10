18f0fcae2c9609a6927063dde44c975f20f49766
alexaNovikova <alexadubinina@mail.ru>
2025-03-10 12:30:27 +0700

    ДЗ к уроку 16 (Сериализация)

--- L16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ L16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,13 +1,24 @@
 package ru.otus.dataprocessor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
 
-    public FileSerializer(String fileName) {}
+    private final ObjectMapper objectMapper;
+    private final String fileName;
+
+    public FileSerializer(String fileName) {
+        this.fileName = fileName;
+        objectMapper = JsonMapper.builder().build();
+    }
 
     @Override
-    public void serialize(Map<String, Double> data) {
-        // формирует результирующий json и сохраняет его в файл
+    public void serialize(Map<String, Double> data) throws IOException {
+        var file = new File(fileName);
+        objectMapper.writeValue(file, data);
     }
 }
