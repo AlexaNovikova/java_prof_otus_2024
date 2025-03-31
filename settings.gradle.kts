f2bfb6eac21c633d811042c37ca5cbe0d982d20d
alexaNovikova <alexadubinina@mail.ru>
2025-03-31 09:52:07 +0700

    Lessons 19, 20, 21

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
 rootProject.name = "java_prof_otus_2024"
 
+
 include("L01-gradle")
 
 include("L02-gradle2")
@@ -33,6 +34,13 @@ include("L17-nio")
 
 include("L18-jdbc:demo")
 include("L18-jdbc:homework")
+include("L19-rdbms")
+
+include("L20-hibernate")
+
+include("L21-jpql:class-demo")
+include("L21-jpql:homework-template")
+
 
 pluginManagement {
     val jgitver: String by settings
